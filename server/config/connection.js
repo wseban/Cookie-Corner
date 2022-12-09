cc3969646bf7b02ce0f7eab23301db4bed87c436
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-12-08 20:09:09 -0800

    Created connection between server and mongoDB

---
+++ server/config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cookieCorner';
+
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+module.exports = mongoose.connection;
