fb7a5effeef6ff42cd83cb99261441a9ef927e56
Priya B <priya_saran@yahoo.com>
2022-12-09 17:30:55 -0800

    Fixes after testing user routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -2,14 +2,16 @@ const router = require('express').Router();
 
 const {
   createUser,
-  getUser,
+  getOneUser,
+  getUsers,
   loginUser,
   logoutUser
 } = require('../../controllers/userController');
 
 router.route('/').post(createUser);
-router.route('/:userId').get(getUser);
+router.route('/').get(getUsers);
+router.route('/:userId').get(getOneUser);
 router.route('/login').post(loginUser);
-router.route('/logout', logoutUser);
+router.route('/logout').post(logoutUser);
 
 module.exports = router;
