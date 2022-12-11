58383a0203f3cc99e8e42870294d10181a416cfd
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-12-10 17:25:47 -0800

    Edited homepage

--- client/src/Components/pages/Home.js
+++ client/src/Components/pages/Home.js
@@ -5,13 +5,18 @@ import CookieMonster from "../assets/CookieMonster.png";
 export default function Home() {
 
     return (
-        <div className="d-flex flex-row justify-content-around align-items-center pt-3">
-            <div>
-            <img className="pt-3" style={{maxWidth: "300px" }} src={CookieMonster} alt="CookieMonster" ></img>
+        <div className="container pt-5">
+            <div className="d-flex flex-row align-items-center">
+            <div className="col-6">
+            <img className="pt-5" style={{maxWidth: "350px" }} src={CookieMonster} alt="CookieMonster" ></img>
             </div>
-            <div className="pt-3" style={{ color: "#504A6D" }}>
-                <h2 className=""> <b>Our Story</b> </h2>
-                <p className=""> Eveything started with a dream...</p>
+            <div className="col-6 pt-5" style={{ color: "#504A6D" }}>
+                <h2 style={{ fontSize:"350%"}}> <b>Our Story</b> </h2>
+                <p style={{ fontSize:"150%"}}> Eveything started with a dream... A dream of baking delicious cookies everyone
+                will love. With authentic cookie recipes that have stood the test of time, we have something delicious 
+                cooking for you. You are only a click away from the most delicious cookie you have ever had. 
+                Act quick though, get your cookie before the Cookie Monster eats it all. </p>
+            </div>
             </div>
         </div>
     )
