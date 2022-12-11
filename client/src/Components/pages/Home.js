484ea34ed457100698eae0eb8411f01b840373b4
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-12-10 16:47:22 -0800

    Edited homepage

--- client/src/Components/pages/Home.js
+++ client/src/Components/pages/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
+import CookieMonster from "../assets/CookieMonster.png";
 
 
-export default function Home(){
+export default function Home() {
 
     return (
-        <div>
-            Home
+        <div className="d-flex flex-row justify-content-around align-items-center pt-3">
+            <div>
+            <img className="pt-3" style={{maxWidth: "300px" }} src={CookieMonster} alt="CookieMonster" ></img>
+            </div>
+            <div className="pt-3" style={{ color: "#504A6D" }}>
+                <h2 className=""> <b>Our Story</b> </h2>
+                <p className=""> Eveything started with a dream...</p>
+            </div>
         </div>
     )
-    
-    }
+
+}
