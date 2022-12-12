929c62efbee5d3611741876a7407e29a4eaaf446
NG <nicholeguan2@gmail.com>
2022-12-12 15:38:20 -0800

    add local db to connection strong and app use routes

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb+srv://userone:passdb12345@webdata.pihvn5i.mongodb.net/?retryWrites=true&w=majority';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cookieCorner';
+// 'mongodb+srv://userone:passdb12345@webdata.pihvn5i.mongodb.net/?retryWrites=true&w=majority';
 
 
 mongoose.connect(connectionString, {
