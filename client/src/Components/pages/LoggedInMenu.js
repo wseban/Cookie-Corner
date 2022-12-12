44433e26bb9313fc119766f9f3c63ccb1058c531
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-12-12 12:56:29 -0800

    Edited loggedin menu page

--- client/src/Components/pages/LoggedInMenu.js
+++ client/src/Components/pages/LoggedInMenu.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { getAllFoods } from '../../utils/api';
 import BrownChocolateChipCookie from "../assets/BrownChocolateChipCookie.png";
 import CherryCheesecakeCookie from "../assets/CherryCheesecakeCookie.png";
 import ChocolateCookie from "../assets/ChocolateCookie.png";
@@ -7,54 +8,60 @@ import PeanutButterCookie from "../assets/PeanutButterCookie.png";
 import WalnutCookie from "../assets/WalnutCookie.png";
 import { BsFillBagDashFill, BsFillBagPlusFill } from "react-icons/bs";
 
-const cookies = [
-    {
-        name: "Brown Chocolate Chip Cookie",
-        price: 7.5,
-        ingredients: "Browned butter, Egg, Brown sugar, Baking soda, Vanilla",
-        picture: BrownChocolateChipCookie
-    },
-    {
-        name: "Cherry Cheesecake Cookie",
-        price: 10,
-        ingredients: "Cherry pie filling, Egg, White sugar, Baking soda, Vanilla",
-        picture: CherryCheesecakeCookie
-
-    },
-    {
-        name: "Chocolate Cookie",
-        price: 5,
-        ingredients: "Chocolate chips, Egg, White sugar, Baking soda, Vanilla",
-        picture: ChocolateCookie
-
-    },
-    {
-        name: "Peanut Butter Cookie",
-        price: 7.5,
-        ingredients: "Creamy full-fat peanut butter, Egg, White sugar, Baking soda, Vanilla",
-        picture: PeanutButterCookie
-    },
-    {
-        name: "Walnut Cookie",
-        price: 5,
-        ingredients: "Chopped walnuts, Egg, White sugar, Baking soda, Vanilla",
-        picture: WalnutCookie
-    }
-];
-
 
 export default function LoggedInMenu() {
+
+    const [cookieData, setCookieData] = useState([]);
+    const cookieImg = [
+        BrownChocolateChipCookie,
+        CherryCheesecakeCookie,
+        ChocolateCookie,
+        PeanutButterCookie,
+        WalnutCookie
+    ]
+
+    useEffect(() => {
+        const getAllCookies = async () => {
+            const response = await getAllFoods();
+            console.log(response);
+            if (!response.ok) {
+                console.log(response.ok);
+                return;
+            }
+
+            const cookieData = await response.json();
+            if (cookieData) {
+
+
+                setCookieData(cookieData);
+                for (let i = 0; i < cookieData.length; i++) {
+                    cookieData[i].picture = getImportName(cookieData[i].picture);
+                }
+                console.log(cookieData);
+            }
+        }
+
+        getAllCookies();
+    }, []);
+
+    function getImportName(picturePath) {
+        const arr = picturePath.split('/');
+        const importName = arr[2].split('.');
+        console.log(importName);
+        return importName[0];
+    }
+
     return (
         <Container>
-            {cookies.map((item) => {
+            {cookieData.map((item, i) => {
                 return (
                     <Container className="mb-30 col-9 p-2">
                         <Row>
                             <Col className="col-12">
-                                <Card style={{ backgroundColor: "#DBBDC7"}} className="m-1">
+                                <Card style={{ backgroundColor: "#DBBDC7" }} className="m-1">
                                     <Card.Body>
                                         <Card.Title style={{ fontSize: "150%", color: "#504A6D" }}>
-                                         {item.name}
+                                            {item.name}
                                         </Card.Title>
                                         <Card.Text style={{ fontSize: "125%", color: "#504A6D" }}>
                                             Price: $ {item.price}
@@ -62,11 +69,11 @@ export default function LoggedInMenu() {
                                         <Card.Text style={{ fontSize: "125%", color: "#504A6D" }}>
                                             Ingredients: {item.ingredients}
                                         </Card.Text>
-                                        <Card.Img className="mr-5" style={{maxWidth: "250px"}} src={item.picture} />
-                                        <Button className="border-0 m-2" style={{ backgroundColor: "#504A6D"}} active>
+                                        <Card.Img className="mr-5" style={{ maxWidth: "250px" }} src={cookieImg[i]} />
+                                        <Button className="border-0 m-2" style={{ backgroundColor: "#504A6D" }} active>
                                             <BsFillBagDashFill color="#eaded2" size={30} />
                                         </Button>
-                                        <Button className="border-0 m-2" style={{ backgroundColor: "#504A6D"}} active>
+                                        <Button className="border-0 m-2" style={{ backgroundColor: "#504A6D" }} active>
                                             <BsFillBagPlusFill color="#eaded2" size={30} />
                                         </Button>
                                     </Card.Body>
