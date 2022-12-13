2b1aca8a4e7a207b058093ca161a63bfca7c1bc4
NG <nicholeguan2@gmail.com>
2022-12-13 13:55:08 -0800

    another deploy test

--- server/server.js
+++ server/server.js
@@ -9,12 +9,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}!`);
