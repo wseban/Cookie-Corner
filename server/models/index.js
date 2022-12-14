fab097285bed7193aab14d5c880679fd2f29054c
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-12-14 11:33:35 -0800

    Fixed bugs

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const Order = require('./Order');
 const Food = require('./Food');
 const User = require('./User');
-const Quantity = require('./Quantity');
 
-module.exports = { User, Food, Order, Quantity };
+module.exports = { User, Food, Order };
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const orderRoutes = require('./orderRoutes');
 const userRoutes = require('./userRoutes');
 const foodRoutes = require('./foodRoutes');
-const quantityRoutes = require('./quantityRoutes');
 
 router.use('/users', userRoutes);
 router.use('/order', orderRoutes);
 router.use('/cookies', foodRoutes);
-router.use('/quantity', quantityRoutes);
 
 module.exports = router;
