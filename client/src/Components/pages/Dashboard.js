4b6e7211b2fa7423b53c7fd47353e47b5c8aa4fe
Will S <fitnesswill13@gmail.com>
2022-12-14 09:44:44 -0800

    sweet alert

--- client/src/Components/pages/Dashboard.js
+++ client/src/Components/pages/Dashboard.js
@@ -4,7 +4,7 @@ import { getMyInfo, deleteOrder } from '../../utils/api';
 import AuthService from '../../utils/auth';
 import { FaTrashAlt } from 'react-icons/fa';
 import { FaEdit } from 'react-icons/fa';
-// import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 /*
 const testOrders = [
@@ -45,6 +45,24 @@ export default function Dashboard() {
   const userDataLength = Object.keys(userData).length;
   console.log("userDataLength 1: " + userDataLength);
 
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+  Toast.fire({
+    icon: 'success',
+    title: 'Signed in successfully'
+  })
+  
   useEffect(() => {
     const getMyOrders = async () => {
       console.log("in getMyOrders()");
@@ -90,22 +108,6 @@ export default function Dashboard() {
         setOrders(userData.orders);
         console.log('name' + userData.fullName);
         console.log('orders = ' + JSON.stringify(userData.orders));
-        // const Toast = Swal.mixin({
-        //   toast: true,
-        //   position: 'top-end',
-        //   showConfirmButton: false,
-        //   timer: 3000,
-        //   timerProgressBar: true,
-        //   didOpen: (toast) => {
-        //     toast.addEventListener('mouseenter', Swal.stopTimer)
-        //     toast.addEventListener('mouseleave', Swal.resumeTimer)
-        //   }
-        // })
-        
-        // Toast.fire({
-        //   icon: 'success',
-        //   title: 'Signed in successfully'
-        // })
         
       }
     }
